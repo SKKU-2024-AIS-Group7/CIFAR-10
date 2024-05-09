b68ba2a49804af4cd8c41c307e9698c66ed7a8a8
jspark2000 <idpjs2000@gmail.com>
2024-05-09 01:09:05 +0000

    implements resnet

--- setup.sh
+++ setup.sh
@@ -2,4 +2,13 @@
 
 set -ex
 
+# Download Dependencies
 pip3 install -r ./requirements.txt
+
+# Download CIFAR-10 Dataset
+url="https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+destination_folder="./data"
+mkdir -p "$destination_folder"
+filename=$(basename "$url")
+wget -O "$destination_folder/$filename" "$url"
+echo "Data successfully downloaded at $destination_folder"
